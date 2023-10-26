d139d6e048986c839b5a8e21f2c689fa755a43e4
PomergranateFan <dmitry1680490@gmail.com>
2023-10-26 17:32:08 +0300

    add TREE function

--- Tree_function.py
+++ Tree_function.py
@@ -6,6 +6,6 @@ def TREE(x, y):
         return f"SUB({x}, {y})"
     else:
         i_s, i_e, j_s, j_e = lcs_result  # Извлекаем индексы начала и конца LCS
-        left_tree = TREE3(x[:i_s], y[:j_s])
-        right_tree = TREE3(x[i_e:], y[j_e:])
+        left_tree = TREE(x[:i_s], y[:j_s])
+        right_tree = TREE(x[i_e:], y[j_e:])
         return f"({left_tree}, ({i_s}, {len(x) - i_e}), {right_tree})"
