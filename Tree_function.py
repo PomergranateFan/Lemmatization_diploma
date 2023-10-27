afb0202880998519180d190de94d08c6b4d315e9
PomergranateFan <dmitry1680490@gmail.com>
2023-10-27 09:07:27 +0300

    Переписал функцию TREE, чтобы она выдавала tuple, а не строку

--- Tree_function.py
+++ Tree_function.py
@@ -1,11 +1,10 @@
-def TREE(x, y):
-    lcs_result = LCS(x, y)  # Получаем результат LCS
+def TREE_tuple(x, y):
+    lcs_result = LCS(x, y)
 
     if lcs_result is None:
-        # Если LCS вернула None, это означает отсутствие общей подпоследовательности
-        return f"SUB({x}, {y})"
+        return (x, y)
     else:
-        i_s, i_e, j_s, j_e = lcs_result  # Извлекаем индексы начала и конца LCS
-        left_tree = TREE(x[:i_s], y[:j_s])
-        right_tree = TREE(x[i_e:], y[j_e:])
-        return f"({left_tree}, ({i_s}, {len(x) - i_e}), {right_tree})"
+        i_s, i_e, j_s, j_e = lcs_result
+        left_tree = TREE_tuple(x[:i_s], y[:j_s])
+        right_tree = TREE_tuple(x[i_e:], y[j_e:])
+        return (left_tree, i_s, right_tree, len(x) - i_e)
