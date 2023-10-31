f5072518286f0969b1ccdc7909ac5c9ccbc035f8
PomergranateFan <dmitry1680490@gmail.com>
2023-10-31 20:09:59 +0300

    Изменил функцию tree так, чтобы работала с линейным lcs

--- Tree_function.py
+++ Tree_function.py
@@ -1,8 +1,9 @@
-from LCS_function import LCS
+#from LCS_function import longest_common_subsequence
+from linear_LCS import linear_longest_common_substring
 
 def TREE_tuple(x, y):
-    lcs_result = LCS(x, y)
-
+    #lcs_result = longest_common_subsequence(x, y)
+    lcs_result = linear_longest_common_substring(x, y)
     if lcs_result is None:
         return (x, y)
     else:
@@ -10,3 +11,13 @@ def TREE_tuple(x, y):
         left_tree = TREE_tuple(x[:i_s], y[:j_s])
         right_tree = TREE_tuple(x[i_e:], y[j_e:])
         return (left_tree, i_s, right_tree, len(x) - i_e)
+
+'''
+# Пример использования
+x = "umgeschaut"
+y = "umschauen"
+print(TREE_tuple(x,y))
+
+'''
+
+
