214d36920924dacb591dcd8dc361ff2b26f543eb
PomergranateFan <dmitry1680490@gmail.com>
2023-11-23 18:29:06 +0300

    переделал класс процессора по-умному

--- LemmaWordformProcessor_class.py
+++ LemmaWordformProcessor_class.py
@@ -2,26 +2,10 @@ from suffix_trees import STree
 
 
 class LemmaWordformProcessor:
-    def __init__(self, lemma, wordform):
+    def __init__(self):
         """
         Конструктор класса LemmaWordformProcessor.
-
-        Args:
-            lemma (str): Лемма.
-            wordform (str): Словоформа.
         """
-        self.lemma = lemma
-        self.wordform = wordform
-        self.tree = None
-
-    def set_tree(self, new_tree):
-        """
-        Метод для установки нового дерева.
-
-        Args:
-            new_tree (object): Новое дерево правил в формате tuple.
-        """
-        self.tree = new_tree
 
     def linear_longest_common_substring(self, x, y):
         """
@@ -49,13 +33,6 @@ class LemmaWordformProcessor:
 
         return start_index_x, end_index_x, start_index_y, end_index_y
 
-    def tree_method(self):
-        """
-        Метод для построения дерева на основе леммы и словоформы.
-        """
-        tree = self.build_tree(self.wordform, self.lemma)
-        self.tree = tree
-
     def build_tree(self, x, y):
         """
         Метод для построения дерева на основе леммы и словоформы.
@@ -77,48 +54,38 @@ class LemmaWordformProcessor:
             right_tree = self.build_tree(x[i_e:], y[j_e:])
             return left_tree, i_s, right_tree, len(x) - i_e
 
-    def get_tree(self):
-        """
-        Метод для получения дерева.
-
-        Returns:
-            object: Дерево правил в формате tuple.
-        """
-        return self.tree
-
-    def apply(self, x):
+    def apply(self, tree, x):
         """
         Метод для применения дерева правил к словоформе и получения леммы.
 
         Args:
+            tree (tuple): Дерево разбора
             x (str): Словоформа.
 
         Returns:
             str: Лемма.
         """
-        if isinstance(self.tree, tuple):
-            if len(self.tree) == 4:
-                tree_i, i_l, tree_j, j_l = self.tree
+        if isinstance(tree, tuple):
+            if len(tree) == 4:
+                tree_i, i_l, tree_j, j_l = tree
                 i_l = int(i_l)
                 j_l = int(j_l)
 
                 if len(x) < i_l + j_l:
                     return None
 
-                self.tree = tree_i
-                p = self.apply(x[:i_l])  # Создаем префикс
+                p = self.apply(tree_i, x[:i_l])  # Создаем префикс
                 if p is None:
                     return None
 
-                self.tree = tree_j
-                s = self.apply(x[-j_l:])  # Создаем суффикс
+                s = self.apply(tree_j, x[-j_l:])  # Создаем суффикс
                 if s is None:
                     return None
 
                 return p + x[i_l:len(x) - j_l] + s  # Конкатенируем префикс, LCS и суффикс
 
-            elif len(self.tree) == 2:
-                u, v = self.tree
+            elif len(tree) == 2:
+                u, v = tree
                 if x == u:
                     return v
                 else:
@@ -130,12 +97,11 @@ class LemmaWordformProcessor:
 lemma = "umschauen"
 wordform = "umgeschaut"
 
-processor = LemmaWordformProcessor(lemma, wordform)
-processor.tree_method()
-print(processor.get_tree())
-result = processor.apply("umgeschaut")
+processor = LemmaWordformProcessor()
+tree = processor.build_tree(wordform, lemma)
+result = processor.apply(tree, "umgeschaut")
 print(f"Lemma: {result}")
+print(tree)
 '''
-
 
 
