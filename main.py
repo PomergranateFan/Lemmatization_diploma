8b042f08985199d1581f6f21180e26d8cd401777
PomergranateFan <dmitry1680490@gmail.com>
2023-11-23 17:32:47 +0300

    посчитал количевство дереьев - 5466

--- main.py
+++ main.py
@@ -1,29 +1,15 @@
 import pickle
 from LemmaWordformProcessor_class import LemmaWordformProcessor
-'''
 
 # Открываем файл для чтения
-with open('tree_set_new_test.pkl', 'rb') as file:
+with open('tree_set_full.pkl', 'rb') as file:
     tree_set = pickle.load(file)
 
-# Выводим первые 10 объектов
-for i, tree in enumerate(tree_set):
-    if i < 10:
-        print(f"Object {i + 1}: {tree}")
-    else:
-        break
+# Получаем количество элементов в объекте tree_set
+num_elements = len(tree_set)
+print(f"Количество элементов: {num_elements}")
 
 # Закрываем файл
 file.close()
 
-'''
-x = "Козел"
-y = "Козла"
-
-processor = LemmaWordformProcessor(x, y)
-processor.TREE()
-print(processor.get_tree())
-
-result = processor.APPLY("ежа")
-print(f"Lemma: {result}")
 
