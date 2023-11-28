8887c08ea35d4214dc6ec850e0cc409d2738ecc8
PomergranateFan <dmitry1680490@gmail.com>
2023-11-28 20:52:40 +0300

    добавил новый класс для другого словаря

--- DictionaryProcessor_class.py
+++ DictionaryProcessor_class.py
@@ -98,9 +98,60 @@ class OpenCorpora(BaseDictionaryCorpus):
 
         print("Extracted and saved lemma-wordform pairs.")
 
+class Unimorph(BaseDictionaryCorpus):
+    def __init__(self, data_path, input_file, output_file):
+        """
+        Инициализация класса OpenCorpora для работы с данными OpenCorpora.
+
+        Args:
+            data_path (str): Путь к исходным данным OpenCorpora (XML-файл словаря OpenCorpora).
+            input_file (str): Путь к файлу, в котором будут сохранены пары лемма-словоформа.
+            output_file (str): Путь к файлу, в котором будут сохранены деревья.
+        """
+        super().__init__(data_path, input_file, output_file)
+
+    def extract_lemma_wordform_pairs(self):
+        """
+        Метод для извлечения пар лемма-словоформа из исходного txt-файла словаря unimorph.
+
+        Проходит через файл и извлекает пары лемма-словоформа.
+
+        Returns:
+            None
+        """
+
+        # Открываем файл для чтения
+        with open(self.data_path, 'r', encoding='utf-8') as file:
+            # Создаем set для хранения уникальных пар лемма-словоформа
+            unique_pairs = set()
+
+            # Читаем строки файла
+            for line in file:
+                # Разделяем строку по символу табуляции
+                parts = line.strip().split('\t')
+
+                # Проверяем, что в строке есть три части
+                if len(parts) == 3:
+                    lemma, wordform, _ = parts  # Извлекаем лемму и словоформу
+
+                    # Добавляем уникальные пары в set
+                    unique_pairs.add((lemma, wordform))
+
+        # Открываем файл для записи в бинарном режиме с использованием pickle
+        with open('self.input_file', 'wb') as output_file:
+
+            pickle.dump(unique_pairs, output_file)
+
+        print("Extracted and saved lemma-wordform pairs.")
+
+
 # Пример использования
 # opencorpora = OpenCorpora("dict.opcorpora_0_82.xml", "lemma_wordform_pair_last.pkl", "tree_set_new.pkl")
 # opencorpora.extract_lemma_wordform_pairs()
 # opencorpora.build_trees()
 # opencorpora.save_tree_set()
 
+unimorph = Unimorph("rus.txt", "pairs_from_unimorph.pkl", "tree_set_from_unimorph_test.pkl" )
+unimorph.extract_lemma_wordform_pairs()
+unimorph.build_trees()
+unimorph.save_tree_set()
