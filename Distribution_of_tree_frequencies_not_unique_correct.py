a37fd11fa04235c4b3dc004ef9c81a0a6d5223e8
PomergranateFan <dmitry1680490@gmail.com>
2023-12-20 02:18:05 +0300

    Поисправлял ошибок в уже неакутальном коде

--- Distribution_of_tree_frequencies_not_unique_correct.py
+++ Distribution_of_tree_frequencies_not_unique_correct.py
@@ -10,10 +10,10 @@ from collections import defaultdict
 with open('tree_set_from_unimorph_test.pkl', 'rb') as f:
     tree_set = pickle.load(f)
 
-with open('pairs_from_unimorph_not_unique.pkl', 'rb') as f:
+with open('pairs_Unimorph_not_unique.pkl', 'rb') as f:
     pairs = pickle.load(f)
 
-# Заводим процессор как элемент класса
+# Заводим процессор как экземпляр класса
 processor = LemmaWordformProcessor()
 
 f_x = defaultdict(int)
@@ -25,18 +25,23 @@ for tree in tree_set:
     for pair in pairs:
         lemma = processor.apply(tree, pair[1])
 
-        # Проверка на потенциальность
-        if lemma is not None:
+        # Проверка на корректность
+        if lemma == pair[0]:
             x_counter += 1  # Увеличиваем счетчик
 
     f_x[x_counter] += 1
+#    if (x_counter in f_x):
+#        f_x[x_counter] += 1
+#    else:
+#        f_x[x_counter] = 1
 
-# Сохранение файла с парами дерево - число неуникальных словоформ по которым лемма потенциальна
-output_file_path = 'tree_numbers_counter_uniform_not_unique.pkl'
+
+# Сохранение файла с парами дерево - число неуникальных словоформ по которым лемма корректна
+output_file_path = 'tree_numbers_counter_uniform_not_unique_potential.pkl'
 with open(output_file_path, 'wb') as output_file:
     pickle.dump(f_x, output_file)
 
-with open('tree_numbers_counter_uniform_not_unique.pkl', 'rb') as f:
+with open('tree_numbers_counter_uniform_not_unique_potential.pkl', 'rb') as f:
     data = pickle.load(f)
 
 # Разделение данных на x и f(x)
@@ -46,13 +51,13 @@ plt.scatter(data.keys(), data.values(), label='log(f(x)) vs log(x)')
 plt.yscale('log')
 plt.xscale('log')
 # Добавление подписей и легенды
-plt.xlabel('log(x)')
-plt.ylabel('log(f(x))')
-plt.title('График f(x) - количество деревьев, которые потенциально строят лемму по неуникальной словоформе только x раз')
+plt.xlabel('x')
+plt.ylabel('f(x)')
+plt.title('График f(x) - количество деревьев, которые корректно строят лемму по неуникальной словоформе только x раз')
 
 plt.legend()
 
 # Показ графика
-plt.savefig('график_как_из_статьи_2_not_unique.png')  # Сохраняем график
+plt.savefig('график_как_из_статьи_2_not_unique_potential.png')  # Сохраняем график
 
 plt.show()
