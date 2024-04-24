b8ae40438d3fa310983be4adedeb01c8e72a40e7
dmitry.gulyaev <dmitry1680490@gmail.com>
2024-04-24 16:31:04 +0300

    Апгрейднул config и разделим метод разделения на классы из udpipe на 2

--- lemma_wordform_processor.py
+++ lemma_wordform_processor.py
@@ -213,13 +213,15 @@ class LemmaWordformProcessorSES(LemmaWordformProcessor):
         return word.replace(' ', '')
 
 
-class LemmaWordformProcessorUD(LemmaWordformProcessor):
-    def __init__(self, allow_copy):
+class LemmaWordformProcessorUD_without_copy(LemmaWordformProcessor):
+    ALLOW_COPY = 0
+    def __init__(self):
         """
         Конструктор класса LemmaWordformProcessorUD.
         """
+
         super().__init__()
-        self.allow_copy = allow_copy
+
         pass
 
     def _min_edit_script(self, source, target, allow_copy):
@@ -254,7 +256,6 @@ class LemmaWordformProcessorUD(LemmaWordformProcessor):
         Параметры:
         - form: строка, представляющая словоформу
         - lemma: строка, представляющая лемму
-        - allow_copy: булево значение, разрешить ли копирование символов
 
         Возвращает строку, представляющую правило лемматизации.
         """
@@ -284,8 +285,8 @@ class LemmaWordformProcessorUD(LemmaWordformProcessor):
             rule += "a" + lemma
         else:
             rule += "d{}¦{}".format(
-                self._min_edit_script(form[:best_form], lemma[:best_lemma], self.allow_copy),
-                self._min_edit_script(form[best_form + best:], lemma[best_lemma + best:], self.allow_copy),
+                self._min_edit_script(form[:best_form], lemma[:best_lemma], self.ALLOW_COPY),
+                self._min_edit_script(form[best_form + best:], lemma[best_lemma + best:], self.ALLOW_COPY),
             )
         return rule
 
@@ -348,9 +349,141 @@ class LemmaWordformProcessorUD(LemmaWordformProcessor):
         return lemma
 
 
+class LemmaWordformProcessorUD_with_copy(LemmaWordformProcessor):
 
+    ALLOW_COPY = 1
+    def __init__(self):
+        """
+        Конструктор класса LemmaWordformProcessorUD.
+        """
 
+        super().__init__()
+        pass
 
+    def _min_edit_script(self, source, target, allow_copy):
+        """
+        Вычисляет минимальный скрипт редактирования для преобразования одной строки в другую.
+
+        Параметры:
+        - source: строка, исходная строка
+        - target: строка, целевая строка
+        - allow_copy: булево значение, разрешить ли копирование символов
+
+        Возвращает строку, представляющую минимальный скрипт редактирования.
+        """
+        a = [[(len(source) + len(target) + 1, None)] * (len(target) + 1) for _ in range(len(source) + 1)]
+        for i in range(0, len(source) + 1):
+            for j in range(0, len(target) + 1):
+                if i == 0 and j == 0:
+                    a[i][j] = (0, "")
+                else:
+                    if allow_copy and i and j and source[i - 1] == target[j - 1] and a[i - 1][j - 1][0] < a[i][j][0]:
+                        a[i][j] = (a[i - 1][j - 1][0], a[i - 1][j - 1][1] + "→")
+                    if i and a[i - 1][j][0] < a[i][j][0]:
+                        a[i][j] = (a[i - 1][j][0] + 1, a[i - 1][j][1] + "-")
+                    if j and a[i][j - 1][0] < a[i][j][0]:
+                        a[i][j] = (a[i][j - 1][0] + 1, a[i][j - 1][1] + "+" + target[j - 1])
+        return a[-1][-1][1]
+
+    def build_rule(self, form, lemma):
+        """
+        Генерирует правило лемматизации для заданной словоформы и ее леммы.
+
+        Параметры:
+        - form: строка, представляющая словоформу
+        - lemma: строка, представляющая лемму
+
+        Возвращает строку, представляющую правило лемматизации.
+        """
+        form = form.lower()
+
+        previous_case = -1
+        lemma_casing = ""
+        for i, c in enumerate(lemma):
+            case = "↑" if c.lower() != c else "↓"
+            if case != previous_case:
+                lemma_casing += "{}{}{}".format("¦" if lemma_casing else "", case,
+                                                i if i <= len(lemma) // 2 else i - len(lemma))
+            previous_case = case
+        lemma = lemma.lower()
+
+        best, best_form, best_lemma = 0, 0, 0
+        for l in range(len(lemma)):
+            for f in range(len(form)):
+                cpl = 0
+                while f + cpl < len(form) and l + cpl < len(lemma) and form[f + cpl] == lemma[l + cpl]: cpl += 1
+                if cpl > best:
+                    best = cpl
+                    best_form = f
+                    best_lemma = l
+
+        rule = lemma_casing + ";"
+        if not best:
+            rule += "a" + lemma
+        else:
+            rule += "d{}¦{}".format(
+                self._min_edit_script(form[:best_form], lemma[:best_lemma], self.ALLOW_COPY),
+                self._min_edit_script(form[best_form + best:], lemma[best_lemma + best:], self. ALLOW_COPY),
+            )
+        return rule
+
+    def apply_rule(self, lemma_rule, form):
+        """
+        Применяет правило лемматизации к словоформе.
+
+        Параметры:
+        - form: строка, представляющая словоформу
+        - lemma_rule: строка, представляющая правило лемматизации
+
+        Возвращает лемматическую форму слова.
+        """
+        if ';' not in lemma_rule:
+            raise ValueError('lemma_rule %r for form %r missing semicolon' % (lemma_rule, form))
+        casing, rule = lemma_rule.split(";", 1)
+        if rule.startswith("a"):
+            lemma = rule[1:]
+        else:
+            form = form.lower()
+            rules, rule_sources = rule[1:].split("¦"), []
+            assert len(rules) == 2
+            for rule in rules:
+                source, i = 0, 0
+                while i < len(rule):
+                    if rule[i] == "→" or rule[i] == "-":
+                        source += 1
+                    else:
+                        assert rule[i] == "+"
+                        i += 1
+                    i += 1
+                rule_sources.append(source)
+
+            try:
+                lemma, form_offset = "", 0
+                for i in range(2):
+                    j, offset = 0, (0 if i == 0 else len(form) - rule_sources[1])
+                    while j < len(rules[i]):
+                        if rules[i][j] == "→":
+                            lemma += form[offset]
+                            offset += 1
+                        elif rules[i][j] == "-":
+                            offset += 1
+                        else:
+                            assert (rules[i][j] == "+")
+                            lemma += rules[i][j + 1]
+                            j += 1
+                        j += 1
+                    if i == 0:
+                        lemma += form[rule_sources[0]: len(form) - rule_sources[1]]
+            except:
+                lemma = form
+
+        for rule in casing.split("¦"):
+            if rule == "↓0": continue  # The lemma is lowercased initially
+            if not rule: continue  # Empty lemma might generate empty casing rule
+            case, offset = rule[0], int(rule[1:])
+            lemma = lemma[:offset] + (lemma[offset:].upper() if case == "↑" else lemma[offset:].lower())
+
+        return lemma
 
 
 '''
